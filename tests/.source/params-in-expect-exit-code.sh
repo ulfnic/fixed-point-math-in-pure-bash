6a3cc5a1d559daa0e2059e9cbb0727ca9e4351af
Marc Gilligan <marcg@ulfnic.com>
2024-06-11 03:19:16 +0000

    tests: add tests for arithmetic addition and subtraction

--- tests/.source/params-in-expect-exit-code.sh
+++ tests/.source/params-in-expect-exit-code.sh
@@ -1,19 +1,40 @@
 params_in_expect_out() {
+	local return_code=0
 
-	./[: "${params_in[@]}" :]
+
+	stdout=$(./[: "${params_in[@]}" :])
 	exit_code=$?
 
-	[[ $exit_code == "$expect_exit_code" ]] && return 0
+	
+	if [[ $exit_code != "$expect_exit_code" ]]; then
+		return_code=1
+		cat <<-EOF
+			TEST ERROR: ${BASH_SOURCE[0]}, unexpected exit code
+			===> Sent
+			${params_in[@]@Q}
+			===> Expected
+			$expect_exit_code
+			===> Received
+			$exit_code
+		EOF
+	fi
 
-	cat <<-EOF
-	TEST ERROR: ${BASH_SOURCE[0]}, unexpected exit code
-	===> Sent
-	${params_in[@]@Q}
-	===> Expected
-	$expect_exit_code
-	===> Received
-	$exit_code
-	EOF
 
-	return 1
+	if [[ -v expect_stdout ]] && [[ $stdout != "$expect_stdout" ]]; then
+		return_code=1
+		cat <<-EOF
+			TEST ERROR: ${BASH_SOURCE[0]}, unexpected stdout
+			===> Sent
+			${params_in[@]@Q}
+			===> Expected
+			$expect_stdout
+			===> Received
+			$stdout
+		EOF
+	fi
+
+	return $return_code
 }
+
+
+
